cc30feecd011b0286af49ebefcfb3400b70f4ce2
Tanishq Chauhan <tanishqchauhan03@gmail.com>
2026-03-28 12:32:51 +0530

    APis

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,13 @@
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, View } from "react-native";
+import { api } from "@/convex/_generated/api";
 
 export default function Index() {
+  const todos = useQuery(api.todos.getTodos);
+  const addTodo = useMutation(api.todos.addTodo);
+  const clearAllTodos = useMutation(api.todos.clearAllTodos);
+
+
   return (
     <View style={Styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
